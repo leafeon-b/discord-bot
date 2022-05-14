c7d2c344a0b22dcbec79d33e17bdb3fdcbe4552c
leafeon-b <grad.f.ishida@gmail.com>
2022-05-14 23:45:56 +0000

    docs:コマンド説明文の修正

--- src/main/main.py
+++ src/main/main.py
@@ -40,7 +40,7 @@ async def shuffle(inter: disnake.AppCmdInter, vc: disnake.VoiceChannel = None):
 
 @bot.slash_command(name="char")
 async def random_character(inter: disnake.AppCmdInter):
-    """Creates random alphabet."""
+    """Creates random Hiragana."""
     characters = ["あ","い","う","え","お","か","き","く","け","こ","さ","し","す","せ","そ","た","ち","つ","て","と","な","に","ぬ","ね","の","は","ひ","ふ","へ","ほ","ま","み","む","め","も","や","ゆ","よ","ら","り","る","れ","ろ","わ","を","ん"]
     c = random.choice(characters)
     await inter.response.send_message(c)
