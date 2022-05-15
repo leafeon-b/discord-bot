354f452382ff6ba4f1fb32a533eb99761a2a52c5
leafeon-b <grad.f.ishida@gmail.com>
2022-05-15 01:49:58 +0000

    feat:全てのコマンドでプレーンテキストではなくEmbedで返すように修正

--- src/main/main.py
+++ src/main/main.py
@@ -47,19 +47,27 @@ async def random_hiragana(inter: disnake.AppCmdInter):
     """Creates random Hiragana."""
     hiraganas = ["あ","い","う","え","お","か","き","く","け","こ","さ","し","す","せ","そ","た","ち","つ","て","と","な","に","ぬ","ね","の","は","ひ","ふ","へ","ほ","ま","み","む","め","も","や","ゆ","よ","ら","り","る","れ","ろ","わ","を","ん"]
     h = random.choice(hiraganas)
-    await inter.response.send_message(h)
+    embed = disnake.Embed(title=h, color=disnake.Colour.dark_gold(), timestamp=datetime.datetime.now(), )
+    embed.set_author(name=inter.author.display_name, icon_url=inter.author.display_avatar)
+    await inter.response.send_message(embed=embed)
 
 
-@bot.slash_command(name="ito", description="Creates random natural number <= 100. This message is shown to only you.")
+@bot.slash_command(name="ito")
 async def random_n(inter: disnake.AppCmdInter):
+    """Creates random natural number <= 100. This message is shown to only you."""
     n = random.randint(1, 100)
-    await inter.response.send_message(n, ephemeral=True)
+    embed = disnake.Embed(title=n, color=disnake.Colour.dark_gold(), timestamp=datetime.datetime.now(), )
+    embed.set_author(name=inter.author.display_name, icon_url=inter.author.display_avatar)
+    await inter.response.send_message(embed=embed, ephemeral=True)
 
 
 @bot.slash_command()
 async def dice(inter: disnake.AppCmdInter, number: commands.Range[1, ...]):
     """Show the result of dice roll only to you."""
-    await inter.response.send_message(random.randint(1, number), ephemeral=True)
+    n = random.randint(1, number)
+    embed = disnake.Embed(title=n, color=disnake.Colour.dark_gold(), timestamp=datetime.datetime.now(), )
+    embed.set_author(name=inter.author.display_name, icon_url=inter.author.display_avatar)
+    await inter.response.send_message(embed=embed, ephemeral=True)
 
 
 @bot.slash_command()
