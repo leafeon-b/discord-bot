910f29ca0395d4d7ba33ad8bf597424be802222a
leafeon-b <grad.f.ishida@gmail.com>
2022-05-15 00:46:08 +0000

    format:自動フォーマット

--- src/main/DenpoView.py
+++ src/main/DenpoView.py
@@ -1,16 +1,15 @@
 from dataclasses import dataclass
 from datetime import datetime
-from time import time
 from typing import ClassVar
-from urllib import response
 
 import disnake
 from disnake import Embed, MessageInteraction, TextInputStyle
 
 
 @dataclass
-class Hint():
+class Hint:
     """TODO: プロセスが走り続ける間はorderが増え続けることでバグが発生するかも"""
+
     tmp_order: ClassVar[int] = 0
 
     author_id: int
@@ -29,6 +28,7 @@ class Hint():
         self.phrase = phrase
         self.order = self.get_tmp_order()
 
+
 class DenpoView(disnake.ui.View):
     embed: Embed
     hints: list[Hint]
@@ -41,7 +41,7 @@ class DenpoView(disnake.ui.View):
     def sort_hint(self) -> None:
         """ヒントのリストを文字数の少ない順、追加の早い順に並び替える"""
         self.hints.sort(key=lambda hint: (len(hint.phrase), hint.order))
-    
+
     def append_hint(self, hint: Hint):
         self.hints.append(hint)
         self.sort_hint()
@@ -49,13 +49,10 @@ class DenpoView(disnake.ui.View):
 
         self.sort_hint()
         self.update_embed()
-    
+
     def create_embed(self, *, title: str, description: str) -> disnake.Embed:
         return disnake.Embed(
-            title=title,
-            description=description,
-            color=disnake.Colour.blurple(),
-            timestamp=datetime.now()
+            title=title, description=description, color=disnake.Colour.blurple(), timestamp=datetime.now()
         )
 
     def update_embed(self):
@@ -67,39 +64,32 @@ class DenpoView(disnake.ui.View):
     async def show_next_hint(self, inter: disnake.MessageInteraction, index: int = 0):
         async def next_hint_callback(tmp_inter: disnake.MessageInteraction):
             await self.show_next_hint(tmp_inter, index + 1)
+
         next_hint_button = disnake.ui.Button(label="次のヒントを表示")
         next_hint_button.callback = next_hint_callback
-        if index == len(self.hints) - 1: # 最後のヒントでは次のヒントを表示ボタンを非アクティブにする
+        if index == len(self.hints) - 1:  # 最後のヒントでは次のヒントを表示ボタンを非アクティブにする
             next_hint_button.disabled = True
 
         async def all_hint_callback(tmp_inter: disnake.MessageInteraction):
-            next_hint_button.disabled = True # 全てのヒントを表示する時は次のヒントを表示ボタンを非アクティブにする
+            next_hint_button.disabled = True  # 全てのヒントを表示する時は次のヒントを表示ボタンを非アクティブにする
             description = ""
             for hint in self.hints:
                 description += f"{hint.phrase} ({hint.author})\n"
-            embed = self.create_embed(
-                title="All Hints",
-                description=description
-            )
+            embed = self.create_embed(title="All Hints", description=description)
             await tmp_inter.response.edit_message(view=view, embed=embed)
+
         all_hint_button = disnake.ui.Button(label="全てのヒントを表示")
         all_hint_button.callback = all_hint_callback
-        
+
         view = disnake.ui.View()
         view.add_item(next_hint_button)
         view.add_item(all_hint_button)
 
-
-
         description = ""
         for i in range(index + 1):
             description += f"{self.hints[i].phrase} ({self.hints[i].author})\n"
-        embed = self.create_embed(
-            title="Denpo!!",
-            description=description
-        )
+        embed = self.create_embed(title="Denpo!!", description=description)
         await inter.response.edit_message(view=view, embed=embed)
-
 
     @disnake.ui.button(label="ヒントを送信")
     async def start(self, button: disnake.ui.Button, inter: disnake.MessageInteraction):
@@ -115,11 +105,11 @@ class DenpoView(disnake.ui.View):
                 child.disabled = True
                 await inter.message.edit(view=self)
         await self.show_next_hint(inter)
-    
+
     @disnake.ui.button(label="ゲームを終了")
     async def exit(self, button: disnake.ui.Button, inter: disnake.MessageInteraction):
         await inter.message.edit(content="ゲームを終了しました.", view=None, embed=None)
-    
+
 
 class DenpoModal(disnake.ui.Modal):
     inter: disnake.MessageInteraction
@@ -157,7 +147,7 @@ class DenpoModal(disnake.ui.Modal):
                 await self.inter.message.edit(view=self.view, embed=self.view.embed)
             embed.add_field(
                 name="Your Hint",
-                value=value[:1024], # 1024文字が上限
+                value=value[:1024],  # 1024文字が上限
                 inline=False,
             )
         await inter.response.send_message(embed=embed, ephemeral=True)
