c5518987e9cfbd1e2177e0d36c964ab9b8fb2612
leafeon-b <grad.f.ishida@gmail.com>
2022-05-27 19:35:38 +0000

    refactor:MyEmbed生成時のdescriptionをオプション化

--- src/main/MyEmbed.py
+++ src/main/MyEmbed.py
@@ -5,6 +5,6 @@ from disnake.embeds import _EmptyEmbed
 
 
 class MyEmbed(disnake.Embed):
-    def __init__(self, *, inter: disnake.AppCmdInter, title: str, color: int | disnake.Colour | _EmptyEmbed):
-        super().__init__(title=title, color=color, timestamp=datetime.now())
+    def __init__(self, *, inter: disnake.AppCmdInter, title: str, description: str = None, color: int | disnake.Colour | _EmptyEmbed):
+        super().__init__(title=title, color=color, timestamp=datetime.now(), description=description)
         self.set_author(name=inter.author.display_name, icon_url=inter.author.display_avatar)
