2ad2d549d6c1e6da332089159e4a744052905684
leafeon-b <grad.f.ishida@gmail.com>
2023-03-17 16:05:45 +0000

    コマンド名修正

--- src/main/cogs/random_n.py
+++ src/main/cogs/random_n.py
@@ -13,7 +13,7 @@ class RandomN(commands.Cog):
     async def on_ready(self):
         print(f"{__name__} cog loaded.")
 
-    @commands.slash_command(name="random")
+    @commands.slash_command(name="random_n")
     async def random_n(inter: disnake.AppCmdInter, min: commands.Range[2, ...] = 1, max: commands.Range[2, ...] = 10):
         """指定した最小値と最大値の範囲内の自然数をランダム生成(初期値は2~10)"""
         if max < min:
